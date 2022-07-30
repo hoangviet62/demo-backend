afe02f0e4ba4c09e3dd6c1bea217795c869f08d0
Tran Hoang Viet Nguyen <viet.nguyen@innogram.co>
2022-07-30 17:43:21 +0700

    update

--- app/jobs/cache_page_worker.rb
+++ app/jobs/cache_page_worker.rb
@@ -5,7 +5,6 @@ class CachePageWorker
   sidekiq_options queue: :cache_page_worker, retry: 0
 
   def perform(key, params)
-    CachingService.new.del(key)
     params = JSON.parse params, symbolize_names: true
     params.each do |article|
       ::Fetch::DetailArticle.new(id: article[:id], url: article[:url]).call.each do |(k, v)|
