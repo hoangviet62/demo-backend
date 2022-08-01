340187c710128e2f6d3e57a536be29af42007507
Tran Hoang Viet Nguyen <viet.nguyen@innogram.co>
2022-08-01 14:36:50 +0700

    update

--- app/jobs/scheduler_worker.rb
+++ app/jobs/scheduler_worker.rb
@@ -5,6 +5,6 @@ class SchedulerWorker
   sidekiq_options queue: :default, retry: 0
 
   def perform
-    1.upto(ENV.fetch("TOTAL_CACHE_PAGE", 1)) { |page| CrawlPageWorker.perform_async(page) }
+    1.upto(ENV.fetch("TOTAL_CACHE_PAGE", 1).to_i) { |page| CrawlPageWorker.perform_async(page) }
   end
 end
